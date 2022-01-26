f09c15b3b7ce2af184fa438d65da7b037c95224d
Владислав Гошко <ferrumflame@gmail.com>
2022-01-26 20:10:11 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/GCD.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Type for finding GCD
+    /// </summary>
+    public class EuclideAlgorithm
+    {
+        /// <summary>
+        /// Method for calculating the GCD of two integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="spentTime">Spent time of calculating algorithm</param>
+        /// <returns>GCD</returns>
+        public int GCD(int a, int b, out int spentTime)
+        {
+            if (a == 0 || b == 0)
+                throw new Exception("One of the numbers is zero");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a = a % b;
+                else
+                    b = b % a;
+            }
+            stopwatch.Stop();
+            spentTime = (int)stopwatch.ElapsedMilliseconds;
+            return a + b;
+        }
+        /// <summary>
+        /// Overloaded method GCD of three integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>GCD of three integers</returns>
+        public int GCD(int a, int b, int c)
+        {
+            if (a == 0 || b == 0 || c == 0)
+                throw new Exception("One of the numbers is zero");
+            return GCD(GCD(a, b, out int spentTime), c, out spentTime);
+        }
+        /// <summary>
+        /// Overloaded method GCD of four integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public int GCD(int a, int b, int c, int d)
+        {
+            if (a == 0 || b == 0 || c == 0 || d == 0)
+                throw new Exception("One of the numbers is zero");
+
+            return GCD(GCD(a, b, out int spentTime), GCD(c, d, out spentTime), out spentTime);
+        }
+        /// <summary>
+        /// Method Binary GCD of two integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="spentTime"></param>
+        /// <returns>GCD</returns>
+        public int BinaryGCD(int a, int b, out int spentTime)
+        {
+            if (a == 0 || b == 0)
+                throw new Exception("One of the numbers is zero");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            int k = 1;
+            while ((a != 0) && (b != 0))
+            {
+                while ((a % 2 == 0) && (b % 2 == 0))
+                {
+                    a /= 2;
+                    b /= 2;
+                    k *= 2;
+                }
+                while (a % 2 == 0) a /= 2;
+                while (b % 2 == 0) b /= 2;
+                if (a >= b) a -= b;
+                else b -= a;
+            }
+            stopwatch.Stop();
+            spentTime = (int)stopwatch.ElapsedMilliseconds;
+            return b * k;
+        }
+        /// <summary>
+        /// Method launchimg methods with different algorithms
+        /// </summary>
+        /// <param name="spentTimeEuclide"></param>
+        /// <param name="spentTimeBinaryGCD"></param>
+        public void GetSpentTimeMethods(out int spentTimeEuclide, out int spentTimeBinaryGCD)
+        {
+            GCD(222222, 111111, out spentTimeEuclide);
+            BinaryGCD(222222, 111111, out spentTimeBinaryGCD);
+        }
+    }
+}
---
+++ ClassLibraryTests/EuclideAlgorithmTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    /// <summary>
+    /// Class test
+    /// </summary>
+    [TestClass()]
+    public class EuclideAlgorithmTests
+    {
+        /// <summary>
+        /// Test method GCD of two integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="gcd"></param>
+        [TestMethod()]
+        [DataRow(99, 123, 3)]
+        [DataRow(99, -123, 3)]
+        [DataRow(41, 6, 1)]
+        public void GCDTest(int a, int b, int gcd)
+        {
+            EuclideAlgorithm euclideAlgorithm = new EuclideAlgorithm();
+            Assert.AreEqual(euclideAlgorithm.GCD(a, b, out int spentTime), gcd);
+        }
+        /// <summary>
+        /// Test method GCD of three integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="gcd"></param>
+        [TestMethod()]
+        [DataRow(99, 999, 81, 9)]
+        [DataRow(99, -123, 3, 3)]
+        [DataRow(41, 6, 256, 1)]
+        public void GCDTest_2(int a, int b, int c, int gcd)
+        {
+            EuclideAlgorithm euclideAlgorithm = new EuclideAlgorithm();
+            Assert.AreEqual(euclideAlgorithm.GCD(a, b, c), gcd);
+        }
+        /// <summary>
+        /// Test method GCD of four integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="gcd"></param>
+        [TestMethod()]
+        [DataRow(99, 123, 3, 54, 3)]
+        [DataRow(99, -123, 3, 5126, 1)]
+        [DataRow(41, 6, 1, 222, 1)]
+        public void GCDTest_3(int a, int b, int c, int d, int gcd)
+        {
+            EuclideAlgorithm euclideAlgorithm = new EuclideAlgorithm();
+            Assert.AreEqual(euclideAlgorithm.GCD(a, b, c, d), gcd);
+        }
+        /// <summary>
+        /// Test method Binary GCD of two integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="gcd"></param>
+        [TestMethod()]
+        [DataRow(99, 123, 3)]
+        [DataRow(99, -123, 3)]
+        [DataRow(-41, 6, 1)]
+        public void BinaryGCDTest(int a, int b, int gcd)
+        {
+            EuclideAlgorithm euclideAlgorithm = new EuclideAlgorithm();
+            Assert.AreEqual(euclideAlgorithm.BinaryGCD(a, b, out int spentTime), gcd);
+        }
+    }
+}
